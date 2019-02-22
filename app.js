cae3fcc3fff749db56dff79c4812bd3df23201c5
Anas RAR <anas00407@gmail.com>
2019-02-22 16:12:51 +0700

    initial commit

--- app.js
+++ app.js
@@ -19,128 +19,150 @@ const querySearch = (q, u) => {
 	return results == null ? null : results[1];
 }
 
-program.command("list").description("Get List Update Anime { Name, Link, and Date }").action(() => {
-
-    figlet('SamehadakuLewatin', (err, data) => {
-        console.log(chalk.yellow(data));
+const myfunc = {
+    init: () => {
+        console.log(chalk.yellow(figlet.textSync('SamehadakuLewatin')));        
         console.log('===============================================================');
-        console.log('🔨  Version    : 1.2.8');
+        console.log('🔨  Version    : 1.2.9');
         console.log('📚  Repository : https://github.com/anasrar/Samehadaku-Lewatin');
         console.log('===============================================================');
-    });
+    },
+    hasil: (url) => {
+        console.log('🌐  URL : '+chalk.blue(url));
+        clipboard.writeSync(url); 
+        console.log(chalk.green('✅  URL Has Copy On Your Clipboard'))
+    },
+    getFromList: (url) => {
 
-    let arrData = [];
+        let arrData = [];
 
-    request(url, (err, res, body) => {
-        if (err && res.statusCode !== 200) throw err;
+        request(url, (err, res, body) => {
+            if (err && res.statusCode !== 200) throw err;
 
-        let $ = cheerio.load(body);
+            let $ = cheerio.load(body);
 
-        $('#tie-block_2598').find('.posts-items>li').each((i, el) => {
-            arrData.push({
-                name: $(el).find('.post-title').text(),
-                value: $(el).find('.post-title>a').attr('href')
-            },
-            new inquirer.Separator('⏲  '+chalk.gray($(el).find('.date.meta-item').text())),
-            new inquirer.Separator('👉  '+chalk.green($(el).find('.post-title>a').attr('href'))),
-            new inquirer.Separator(chalk.red("==========================="))
-            );
-            // console.log(chalk.green($(el).find('.post-title').text()));
-            // console.log(chalk.gray($(el).find('.date.meta-item').text()));
-            // console.log(chalk.blue($(el).find('.post-title>a').attr('href')))
-        });
-    }).then(() => {
-        inquirer.prompt([{
-            type: "list",
-            name: "url",
-            message: "Select The Anime",
-            choices: arrData
-        }]).then((answer) => {
+            $('#tie-block_2598').find('.posts-items>li').each((i, el) => {
+                new inquirer.Separator(chalk.red('============== '+(i+1)+' ==============')),
+                arrData.push({
+                    name: $(el).find('.post-title').text(),
+                    value: $(el).find('.post-title>a').attr('href')
+                },
+                new inquirer.Separator('⏲  '+chalk.gray($(el).find('.date.meta-item').text())),
+                new inquirer.Separator('👉  '+chalk.green($(el).find('.post-title>a').attr('href'))),
+                new inquirer.Separator(chalk.red('============== = =============='))
+                );
+                // console.log(chalk.green($(el).find('.post-title').text()));
+                // console.log(chalk.gray($(el).find('.date.meta-item').text()));
+                // console.log(chalk.blue($(el).find('.post-title>a').attr('href')))
+            });
+        }).then(() => {
+            inquirer.prompt([{
+                type: "list",
+                name: "url",
+                message: "Select The Anime",
+                choices: arrData
+            }]).then((answer) => {
 
-            arrData = [];
+                arrData = [];
 
-            request(answer.url, (err, res, body) => {
-                if (err && res.statusCode !== 200) throw err;
+                request(answer.url, (err, res, body) => {
+                    if (err && res.statusCode !== 200) throw err;
 
-                let $ = cheerio.load(body);
+                    let $ = cheerio.load(body);
 
-                $('#the-post').find('.download-eps').each((i, el) => {
+                    $('#the-post').find('.download-eps').each((i, el) => {
 
-                    arrData.push(
-                    {
-                        name: $(el).prev().text(),
-                        value: $(el).html()
-                    });
-
-                    // console.log($(el).prev().text());
-                    // console.log($(el).html());
-                    // console.log(arrData);
-                });
-
-            }).then(() => {
-                inquirer.prompt([{
-                    type: "list",
-                    name: "format",
-                    message: "Select The Format",
-                    choices: arrData
-                }]).then((answer) => {
-
-                    arrData = [];
-
-                    let $ = cheerio.load(answer.format);
-
-                    $('li').each((i, el) => {
-
-                        let reso = $(el).find('strong').text();
-                        arrData.push(new inquirer.Separator(chalk.green(`== ${reso} ==`)));
-                        $(el).find('a').each((i, el) => {
-                            arrData.push({
-                                name: $(el).text() + ' ['+reso+']',
-                                value: $(el).attr('href')
-                            });
-                            // console.log($(el).attr('href'));
+                        arrData.push(
+                        {
+                            name: $(el).prev().text(),
+                            value: $(el).html()
                         });
+
+                        // console.log($(el).prev().text());
+                        // console.log($(el).html());
+                        // console.log(arrData);
                     });
 
+                }).then(() => {
                     inquirer.prompt([{
                         type: "list",
-                        name: "url",
-                        message: "Select The Resolution and Server ",
+                        name: "format",
+                        message: "Select The Format",
                         choices: arrData
                     }]).then((answer) => {
-                        let next = !1;
-                        request(answer.url, (err, res, body) => {
-                            if (err && res.statusCode !== 200) throw err;
-                            let $ = cheerio.load(body);
-                            let url = $('#splash').find('a[href*="?r=a"]').attr('href');
-                            url = Buffer.from(querySearch('r', url), 'base64').toString('ascii');
-                            next = shorturl.indexOf(extractDomain(url)) !== -1 ? url : false;
-                        }).then((a) => {
-                            if (next) {
-                                request(next, (err, res, body) => {
-                                    if (err && res.statusCode !== 200) throw err;
-                                    let $ = cheerio.load(body);
-                                    let url = $('#splash').find('a[href*="?r=a"]').attr('href');
-                                    url = Buffer.from(querySearch('r', url), 'base64').toString('ascii');
-                                    console.log('🌐  URL : '+chalk.blue(url));
-                                    clipboard.writeSync(url); 
-                                    console.log(chalk.green('✅  URL Has Copy On Your Clipboard'))
-                                    // next = shorturl.indexOf(extractDomain(url)) !== -1 ? url : false;
-                                })
-                            } else {
-                                let $ = cheerio.load(a);
+
+                        arrData = [];
+
+                        let $ = cheerio.load(answer.format);
+
+                        $('li').each((i, el) => {
+
+                            let reso = $(el).find('strong').text();
+                            arrData.push(new inquirer.Separator(chalk.green(`== ${reso} ==`)));
+                            $(el).find('a').each((i, el) => {
+                                arrData.push({
+                                    name: $(el).text() + ' ['+reso+']',
+                                    value: $(el).attr('href')
+                                });
+                                // console.log($(el).attr('href'));
+                            });
+                        });
+
+                        inquirer.prompt([{
+                            type: "list",
+                            name: "url",
+                            message: "Select The Resolution and Server ",
+                            choices: arrData
+                        }]).then((answer) => {
+                            let next = !1;
+                            request(answer.url, (err, res, body) => {
+                                if (err && res.statusCode !== 200) throw err;
+                                let $ = cheerio.load(body);
                                 let url = $('#splash').find('a[href*="?r=a"]').attr('href');
                                 url = Buffer.from(querySearch('r', url), 'base64').toString('ascii');
-                                console.log('🌐  URL : '+chalk.blue(url));
-                                clipboard.writeSync(url); 
-                                console.log(chalk.green('✅  URL Has Copy On Your Clipboard'))
-                            }
+                                next = shorturl.indexOf(extractDomain(url)) !== -1 ? url : false;
+                            }).then((a) => {
+                                if (next) {
+                                    request(next, (err, res, body) => {
+                                        if (err && res.statusCode !== 200) throw err;
+                                        let $ = cheerio.load(body);
+                                        let url = $('#splash').find('a[href*="?r=a"]').attr('href');
+                                        url = Buffer.from(querySearch('r', url), 'base64').toString('ascii');
+                                        myfunc.hasil(url)
+                                        // next = shorturl.indexOf(extractDomain(url)) !== -1 ? url : false;
+                                    })
+                                } else {
+                                    let $ = cheerio.load(a);
+                                    let url = $('#splash').find('a[href*="?r=a"]').attr('href');
+                                    url = Buffer.from(querySearch('r', url), 'base64').toString('ascii');
+                                    myfunc.hasil(url);
+                                }
+                            });
                         });
                     });
                 });
             });
         });
-    });
+
+    }
+}
+
+program.version('🔨  Version    : 1.2.9', '-v, --version');
+
+program.command("list").description("Get List Update Anime { Name, Link, and Date }").action(() => {
+
+    myfunc.init();
+    console.log(chalk.yellow('⏳  Getting List Anime...'));
+    myfunc.getFromList(url);
+
+});
+
+program.command("page <int>").description("Get List Anime From Page { Name, Link, and Date }").action((int) => {
+
+    myfunc.init();
+    console.log(chalk.yellow('⏳  Getting List Anime From Page '+int+'...'));
+    myfunc.getFromList(url+'page/'+int);
+
 });
 
 program.parse(process.argv);
